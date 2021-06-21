d6cba4aa38a47d0b8c710f00040fcc568c29cb25
barrett-shea <barrettkshea@gmail.com>
2021-06-21 14:17:19 -0400

    Prevent empty tasks from being added. Clears input after submit

--- src/AddToDo.js
+++ src/AddToDo.js
@@ -1,15 +1,25 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 export default function AddToDo({ setTodos }) {
+  const [item, setItem] = useState('')
+  
+  function handleChange(e) {
+    e.preventDefault();
+    setItem(e.target.value)
+  }
+  
   function handleSubmit(e) {
     e.preventDefault();
     const todo = e.target.elements.todo.value; // id.value
-    setTodos(prevTodos => [...prevTodos, todo]); // spread all of the previous todos and add new todo as last element in array
+    if (todo) 
+      {setTodos(prevTodos => [...prevTodos, todo]); // spread all of the previous todos and add new todo as last element in array
+      setItem('')
+    }
   }
 
   return (
     <form onSubmit={handleSubmit}>
-      <input type="text" id='todo' />
+      <input onChange={handleChange} value={item} type="text" id='todo' />
       <button type="submit">Add Todo</button>
     </form>
   );
